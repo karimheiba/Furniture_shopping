dab6caa661f15f9210cf8ac573903bd6038fc5eb
karimheiba <kimoheiba@gmail.com>
2022-06-08 00:17:48 +0200

    first commit

---
+++ lib/screen/boarding.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class Boarding extends StatelessWidget {
+  const Boarding({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+    
+  }
+}
